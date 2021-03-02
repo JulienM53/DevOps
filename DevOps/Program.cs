03dba809dd41ffa01b7fc745132705bb1180eb76
aski <craftou53@gmail.com>
2021-03-02 16:53:14 +0100

    Un commit

--- DevOps/Program.cs
+++ DevOps/Program.cs
@@ -6,7 +6,7 @@ namespace DevOps
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bonjour le monde");
         }
     }
 }
