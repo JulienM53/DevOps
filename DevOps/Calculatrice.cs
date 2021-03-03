0b79d87dd958d588fd5f253e13645e53eb24f32d
aski <craftou53@gmail.com>
2021-03-03 12:07:14 +0100

    Ajout de test au projet

---
+++ DevOps/Calculatrice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevOps
+{
+    public class Calculatrice
+    {
+        public Calculatrice()
+        {
+
+        }
+
+        public int Addition(int a, int b)
+        {
+            int r = a + b;
+            return r;
+        }
+
+        public int Soustraction(int a, int b)
+        {
+            int r = a - b;
+            return r;
+        }
+    }
+
+
+}
